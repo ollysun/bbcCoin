be826b6af9b4a6ca0f38389802e5be5b0c27b432
Moses <ollysun@gmail.com>
2017-07-26 05:19:00 +0100

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Coin Project';
+
+  coinsValue = [
+    { "moneyVal": 200, "moneySymbol": '£2' },
+    { "moneyVal": 100, "moneySymbol": '£1' },
+    { "moneyVal": 50, "moneySymbol": '50 pence' },
+    { "moneyVal": 2, "moneySymbol": '2 pence' },
+    { "moneyVal": 1, "moneySymbol": '1 pence' }
+  ];
+
+  moneyVal: any;
+  amountVal: any = this.moneyVal;
+
+  getRootArray() {
+    var valM = this.moneyVal;
+    return this.coinsValue.map(function (coin, i) {
+      return [Math.floor(valM / coin.moneyVal), valM %= coin.moneyVal][0];
+    });
+  }
+
+  getResult = function () {
+    var arrayN = this.getRootArray();
+    var result = [];
+    var coinVal=[];
+    coinVal = this.coinsValue;
+    arrayN.forEach(function(value,index,result) {
+        this.push({coinVal: coinVal[index].moneySymbol, amountVal:value})
+    }, result);
+
+    return result;
+  };
+
+  
+  coinsCalc: any;
+  Submit() {
+    this.coinsCalc = this.getResult();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { OnlyNumber } from 'app/onlynumber.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+     OnlyNumber
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
